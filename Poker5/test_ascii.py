8b2e5944ff6a4037672a88e369608be1f3e95f19
ashidagithub <1019726435@qq.com>
2019-07-23 23:14:50 +0800

    added test_ascii.py

---
+++ Poker5/test_ascii.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+# ------------------------(max to 80 columns)-----------------------------------
+# author by : （学员ID)
+# created:  2019.7.10
+
+# Description:
+#   学习ASCII 码
+# ------------------------(max to 80 columns)-----------------------------------
+
+
+print(chr(65))
+
+for k in range(33,127):
+    print ('ASC CODE (%d) is %c' % (k, chr(k)))
